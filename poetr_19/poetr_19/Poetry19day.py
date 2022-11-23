d46f72c58cbe40ed2ae6e0452044b9298a3aafb1
Andreymazo <andreymazo@mail.ru>
2022-11-23 17:50:57 +0300

    poetr_19/

---
+++ poetr_19/poetr_19/Poetry19day.py
@@ -0,0 +1,16 @@
+import getpass
+import datetime
+
+def main():
+
+    name = getpass.getuser().capitalize()
+    now = datetime.datetime.now()
+    today = now.strftime('%d, %b %Y')
+    time = now.strftime("%H:%M")
+    print('19 day Poetry')
+    print(f'Hello {name}!')
+    print(f'Today is {today}.')
+    print(f'It\'s {time} now.')
+
+if __name__ == '__main__':
+        main()
